af580c4562bcedcdd814f947f7595964cd19c491
abdelilah2005 <abdelilahessemlali@gmail.com>
2025-04-10 16:38:33 +0100

    add component page not found

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import './App.css'
 import { BrowserRouter,Routes, Route } from 'react-router-dom'
 import Home from './Home'
+import Create from './Create'
+import Update from './Update'
+import Read from './Read' 
+import NotFound from './NotFound' 
 
 
 function App() {
@@ -9,7 +13,11 @@ function App() {
   return (
    <BrowserRouter>
      <Routes>
-      <Route path='/' element={<Home/>}></Route>
+      <Route path='/' element={<Home />}></Route>
+      <Route path='/create' element={<Create />}></Route>
+      <Route path='/update/:id' element={<Update />}></Route>
+      <Route path='/read/:id' element={<Read />}></Route>
+      <Route path='*' element={<NotFound />} /> {/* ⬅️ Catch-all */}
      </Routes>
    </BrowserRouter>
   )
